dd9b73f3a45b5012f583a492099b1c82983ce488
bilgisen <hbkarabey@gmail.com>
2025-08-21 00:25:13 +0300

    order

--- app/dashboard/books/[slug]/chapters/page.tsx
+++ app/dashboard/books/[slug]/chapters/page.tsx
@@ -1,15 +1,17 @@
 // app/dashboard/books/[slug]/chapters/page.tsx
 "use client";
 
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import { ChapterTreeWrapper } from "@/components/chapters/ChapterTree";
 
 export default function ChaptersPage({ params }: { params: { slug: string } }) {
-  // slug ile kitabın chapterlarını fetch edip ChapterTreeWrapper'a verebilirsin.
-  // Şimdilik demo olarak initialTree kullanıyor.
   return (
     <div className="p-4">
       <h1 className="text-xl font-semibold mb-4">Chapters for {params.slug}</h1>
-      <ChapterTreeWrapper />
+      <DndProvider backend={HTML5Backend}>
+        <ChapterTreeWrapper />
+      </DndProvider>
     </div>
   );
 }
